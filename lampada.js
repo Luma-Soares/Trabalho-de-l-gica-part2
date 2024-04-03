280f5e80d0fcfa30f846abbba59acc18788d9e27
Luma <152003153+Luma-Soares@users.noreply.github.com>
2024-04-03 14:21:15 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,9 @@
+function Ligar(){
+    var Ligar = document.getElementById('lampada');
+     Ligar.src="../ligada.jpg"
+}
+
+function Desligar(){
+    var Desligar = document.getElementById('lampada');
+      Desligar.src="../desligada.jpg"
+}
